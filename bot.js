b28a09134239bafc6c6bd3b7a26a2d9882851dc7
emre2metre <76652467+emre2metre@users.noreply.github.com>
2021-10-14 11:07:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -168,7 +168,7 @@ ${chalk.blue.italic('ℹ️ Connecting to WhatsApp... Please Wait.')}`);
             chalk.green.bold('✅ Plugins Installed!')
         );
         await new Promise(r => setTimeout(r, 1100));
-        if (conn.user.jid == one) {
+        if (conn.user.jid == ten) {
             await conn.sendMessage(conn.user.jid,nw, MessageType.text), console.log(nw), await new Promise(r => setTimeout(r, 1000))
             await heroku.get(baseURI + '/formation').then(async (formation) => { 
                 forID = formation[0].id; 
