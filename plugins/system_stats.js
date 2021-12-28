87814ab8e9e209deb9fb5462712f5e4a6d39f21d
emre2metre <76652467+emre2metre@users.noreply.github.com>
2021-12-28 21:26:27 +0300

    Update system_stats.js

--- plugins/system_stats.js
+++ plugins/system_stats.js
@@ -23,7 +23,7 @@ if (Config.WORKTYPE == 'private') {
     Asena.addCommand({pattern: 'alive', fromMe: true, desc: Lang.ALIVE_DESC}, (async (message, match) => {
 
         if (Config.ALIVEMSG == 'default') {
-            await message.client.sendMessage(message.jid,'```Tanrı Türk\'ü Korusun. 🐺 Asena Hizmetinde!```\n\n*Version:* ```'+Config.VERSION+'```\n*Branch:* ```'+Config.BRANCH+'```\n*Telegram Group:* https://t.me/AsenaSupport\n*Telegram Channel:* https://t.me/asenaremaster\n*Plugin Channel:* ' + Config.CHANNEL , MessageType.text);
+            await message.client.sendMessage(message.jid,'EMRENİN SEKS KOLESİ HZ.SALEH BOT ÇALIŞIYOR HURRAAAAA' , MessageType.text);
         }
         else {
             const pow = '*Powered by WhatsAsena*'
